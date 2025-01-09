854b1be9d3157bfca3c322d14d1f60241a350ece
Noah Vaters <144738466+NoV8rs@users.noreply.github.com>
2025-01-09 12:00:21 -0400

    Remove the animation code

--- Assets/Scripts/Noah/ActiveLevelChanging.cs
+++ Assets/Scripts/Noah/ActiveLevelChanging.cs
@@ -20,7 +20,6 @@ public class ActiveLevelChanging : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            _animator
             levels[currentLevel].SetActive(false);
             currentLevel++;
             if (currentLevel >= levels.Length)
