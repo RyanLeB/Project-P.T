e8af838afb807463c0c01d9688b036bd5aa8f433
charcurry <charlieryancurry@gmail.com>
2025-01-13 18:25:56 -0400

    add puzzle element to lightswitch

--- Assets/Scripts/Charlie/Lightswitch.cs
+++ Assets/Scripts/Charlie/Lightswitch.cs
@@ -6,10 +6,22 @@ public class Lightswitch : MonoBehaviour
 {
     bool lightOn;
     public GameObject[] lightObjects;
+    public int lastStatesToRemember = 5;
+    public List<bool> lastStates = new List<bool>();
 
     public void ToggleSwitch()
     {
         lightOn = !lightOn;
+        if (lastStates.Count >= lastStatesToRemember)
+        {
+            lastStates.RemoveAt(0);
+        }
+        lastStates.Add(lightOn);
+    }
+
+    private void Start()
+    {
+        lastStates.Add(lightOn);
     }
 
     void Update()
