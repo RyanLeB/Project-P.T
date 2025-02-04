097586cf7257b209df53300a722f1861d33d7b23
Charlie <charlieryancurry@gmail.com>
2025-02-04 13:03:54 -0400

    add a trigger script can be a door and a sound trigger

---
+++ Assets/Scripts/Charlie/Trigger.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    public bool soundTrigger;
+    public bool doorTrigger;
+
+    public Trigger nextTrigger;
+
+    [Header("Sound Trigger Settings")]
+
+    public GameObject soundPosition;
+
+    public AudioClip sound;
+
+    public bool playOnStart = false;
+
+    public bool loop = false;
+
+    public float spacialBlend = 1;
+
+    public float volume = 1;
+
+    [Header("Door Trigger Settings")]
+
+    public Door door;
+
+    public enum DoorActionType
+    {
+        Open,
+        Close,
+        Unlock,
+        Lock,
+        Crack
+    }
+
+    public DoorActionType actionType;
+
+    public void Start()
+    {
+        if (soundTrigger)
+        {
+            soundPosition.AddComponent<AudioSource>();
+            soundPosition.GetComponent<AudioSource>().clip = sound;
+            soundPosition.GetComponent<AudioSource>().playOnAwake = playOnStart;
+            soundPosition.GetComponent<AudioSource>().loop = loop;
+            soundPosition.GetComponent<AudioSource>().spatialBlend = spacialBlend;
+            soundPosition.GetComponent<AudioSource>().volume = volume;
+        }
+
+        if (nextTrigger != null)
+        {
+            nextTrigger.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (soundTrigger)
+        {
+            soundPosition.GetComponent<AudioSource>().Play();
+        }
+        if (doorTrigger)
+        {
+            switch (actionType)
+            {
+                case DoorActionType.Open:
+                    door.Open();
+                    break;
+                case DoorActionType.Close:
+                    door.Close();
+                    break;
+                case DoorActionType.Unlock:
+                    door.UnlockDoor();
+                    break;
+                case DoorActionType.Lock:
+                    door.Lock();
+                    break;
+                case DoorActionType.Crack:
+                    door.Crack();
+                    break;
+            }
+        }
+
+        if (nextTrigger != null)
+        {
+            nextTrigger.gameObject.SetActive(true);
+        }
+
+        Destroy(this);
+    }
+}
+
+[CustomEditor(typeof(Trigger))]
+public class TriggerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        Trigger trigger = (Trigger)target;
+
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("soundTrigger"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("doorTrigger"));
+
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("nextTrigger"));
+
+        if (trigger.soundTrigger)
+        {
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("soundPosition"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("sound"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("playOnStart"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("loop"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("spacialBlend"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("volume"));
+        }
+        if (trigger.doorTrigger)
+        {
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("door"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("actionType"));
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
